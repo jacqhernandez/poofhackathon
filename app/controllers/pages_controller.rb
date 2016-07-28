5dd35e826d4514433f098b45d4510967735792c4
jacqhernandez <jm.hernandez96@yahoo.com>
2016-07-28 06:35:42 -0200

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def prepare
+    @areas = Area.all
   end
 
   def about
@@ -16,10 +17,6 @@ class PagesController < ApplicationController
     @residents = Resident.where(area_id: @area.id)
   end
 
-  def centers
-    @areas = Area.all
-  end
-
   def responses
     @responses = LguResponse.where(lgu_officer_id: current_lgu_officer.id)
   end
@@ -31,28 +28,45 @@ class PagesController < ApplicationController
   def view
     start_date = params['start_date']
     end_date = params['end_date']
-    if (start_date.present? && !end_date.present?)
-      @resident_lgu_responses = ResidentLguResponse.where("created_at >= ?", start_date.to_date).where(is_safe: false)
-      @date_message = start_date
-    elsif (end_date.present? && !start_date.present?)
-      @resident_lgu_responses = ResidentLguResponse.where("created_at >= ?", end_date.to_date).where(is_safe: false)
-      @date_message = end_date
-    elsif (end_date.present? && start_date.present?)
-      @resident_lgu_responses = ResidentLguResponse.where("created_at >= ? AND created_at <= ?", start_date.to_date, end_date.to_date).where(is_safe: false)
-      @date_message = "#{start_date} to #{end_date}"
+    if lgu_officer_signed_in?
+      if (start_date.present? && !end_date.present?)
+        @resident_lgu_responses = ResidentLguResponse.where("resident_lgu_responses.created_at >= ?", start_date.to_date).where(is_safe: false).joins(:lgu_response).merge(LguResponse.where(lgu_officer_id: current_lgu_officer.id))
+        @date_message = start_date
+      elsif (end_date.present? && !start_date.present?)
+        @resident_lgu_responses = ResidentLguResponse.where("resident_lgu_responses.created_at >= ?", end_date.to_date).where(is_safe: false).joins(:lgu_response).merge(LguResponse.where(lgu_officer_id: current_lgu_officer.id))
+        @date_message = end_date
+      elsif (end_date.present? && start_date.present?)
+        @resident_lgu_responses = ResidentLguResponse.where("resident_lgu_responses.created_at >= ?", end_date.to_date).where(is_safe: false).joins(:lgu_response).merge(LguResponse.where(lgu_officer_id: current_lgu_officer.id))
+        @date_message = "#{start_date} to #{end_date}"
+      else
+        @resident_lgu_responses = ResidentLguResponse.joins(:lgu_response).merge(LguResponse.where(lgu_officer_id: current_lgu_officer.id)).where(is_safe: false).where("resident_lgu_responses.created_at >= ?", Time.zone.now.beginning_of_day)
+        @date_message = Date.today
+      end
+      @hash = Gmaps4rails.build_markers(@resident_lgu_responses) do |resident_lgu_response, marker|
+        marker.lat resident_lgu_response.latitude
+        marker.lng resident_lgu_response.longitude
+         marker.infowindow "Location: #{resident_lgu_response.address}<br>Longitude: #{resident_lgu_response.longitude}<br>Latitude: #{resident_lgu_response.latitude}<br>Disaster: #{resident_lgu_response.lgu_response.disaster_type}<br>Name:#{resident_lgu_response.resident.first_name} #{resident_lgu_response.resident.last_name}"
+         marker.picture({
+         "width" =>  38,        
+         "height" => 61
+         })
+      end
+      @evacuation_centers = EvacuationCenterArea.where(area_id: current_lgu_officer.area.id)
+      @health_centers = HealthCenterArea.where(area_id: current_lgu_officer.area.id)
     else
-      @resident_lgu_responses = ResidentLguResponse.where("created_at >= ?", Time.zone.now.beginning_of_day).where(is_safe: false)
+      @resident_lgu_responses = ResidentLguResponse.where(is_safe: false).where("resident_lgu_responses.created_at >= ?", Time.zone.now.beginning_of_day)
       @date_message = Date.today
+      @hash = Gmaps4rails.build_markers(@resident_lgu_responses) do |resident_lgu_response, marker|
+        marker.lat resident_lgu_response.latitude
+        marker.lng resident_lgu_response.longitude
+         marker.infowindow "Location: #{resident_lgu_response.address}<br>Disaster: #{resident_lgu_response.lgu_response.disaster_type}"
+         marker.picture({
+         "width" =>  38,        
+         "height" => 61
+         })
+      end
     end
-    @hash = Gmaps4rails.build_markers(@resident_lgu_responses) do |resident_lgu_response, marker|
-      marker.lat resident_lgu_response.latitude
-      marker.lng resident_lgu_response.longitude
-       marker.infowindow "Location: #{resident_lgu_response.address}<br>Longitude: #{resident_lgu_response.longitude}<br>Latitude: #{resident_lgu_response.latitude}<br>Disaster: #{resident_lgu_response.lgu_response.disaster_type}<br>Name:#{resident_lgu_response.resident.first_name} #{resident_lgu_response.resident.last_name}"
-       marker.picture({
-       "width" =>  38,        
-       "height" => 61
-       })
-    end
+
   end
 
   def contact
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 Region.create(name: "NCR")
 
-Area.create(name: "Makati", province: "Metro Manila", is_city: true)
-Area.create(name: "Muntinlupa", province: "Metro Manila", is_city: true)
+Area.create(name: "Makati", province: "Metro Manila", is_city: true, region_id: 1)
+Area.create(name: "Muntinlupa", province: "Metro Manila", is_city: true, region_id: 1)
 
 LguOfficer.create(username: "admelgar", password: "123456", password_confirmation: "123456", first_name: "Anna", last_name: "Melgar", phone_number: "09171234567", area_id: 1)
 LguOfficer.create(username: "janineladdaran", password: "123456", password_confirmation: "123456", first_name: "Janine", last_name: "Laddaran", phone_number: "09177654321", area_id: 2)
@@ -19,14 +19,14 @@ Resident.create(first_name: "Erika", last_name: "Reyes", address: "SM Makati", p
 Resident.create(first_name: "Alvin", last_name: "Bravo", address: "New BIlibid Prison", phone_number: "09274869965", phone_subscriber: "Globe", area_id: 2)
 Resident.create(first_name: "Dan Ping", last_name: "Cai", address: "Jamboree Lake, Muntinlupa", phone_number: "09154480244", phone_subscriber: "Globe", area_id: 2)
 
-EvacuationCenter.create(name:"Villamor Air Base Golf Course", contact_person: "Toni Garcia", contact_number: "09171234567")
-EvacuationCenter.create(name:"Intramuros Golf Course", contact_person: "Kevin Cua", contact_number: "09171234567")
+EvacuationCenter.create(name:"Villamor Air Base Golf Course", address: "Villamor Golf Club, Colonel Jesus Villamor Air base, Pasay City, Metro Manila", longitude: 121.05012310000001, latitude: 14.5537703, contact_person: "Toni Garcia", contact_number: "09171234567")
+EvacuationCenter.create(name:"Intramuros Golf Course", address: "Bonifacio Drive, Intramuros, Manila", longitude: 121.05012310000001, latitude: 14.5537703, contact_person: "Kevin Cua", contact_number: "09171234567")
 EvacuationCenterArea.create(evacuation_center_id:1, area_id: 1)
 EvacuationCenterArea.create(evacuation_center_id:2, area_id: 1)
 EvacuationCenterArea.create(evacuation_center_id:1, area_id: 2)
 
-HealthCenter.create(name:"Red Cross Marikina", contact_person: "Toni Garcia", contact_number: "09171234567")
-HealthCenter.create(name:"Ultra Pasig", contact_person: "Kevin Cua", contact_number: "09171234567")
+HealthCenter.create(name:"Red Cross Marikina", address: "J. P. Rizal St, Kupang, Marikina, 1820 Metro Manila", longitude: 121.05012310000001, latitude: 14.5537703, contact_person: "Toni Garcia", contact_number: "09171234567")
+HealthCenter.create(name:"Ultra Pasig", address: "PhilSports Arena, Capt. Henry P. Javier, Pasig, Metro Manila", longitude: 121.05012310000001, latitude: 14.5537703, contact_person: "Kevin Cua", contact_number: "09171234567")
 HealthCenterArea.create(health_center_id:1, area_id: 1)
 HealthCenterArea.create(health_center_id:2, area_id: 1)
 HealthCenterArea.create(health_center_id:1, area_id: 2)
